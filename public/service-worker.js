85f3141f3e64cfc82e1efd27155292def46d0dd7
jonyprill <jonathantprill@gmail.com>
2022-04-30 15:27:31 -0500

    adds PWA

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,42 @@
 const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
 
 const FILES_TO_CACHE = [
+    "/",
     "./index.html",
     "./css/styles.css",
-    "./js/index.js"
+    './js/idb.js',
+    "./js/index.js",
+    './manifest.json',
+    './icons/icon-512x512.png',
+    './icons/icon-384x384.png',
+    './icons/icon-192x192.png',
+    './icons/icon-152x152.png',
+    './icons/icon-144x144.png',
+    './icons/icon-128x128.png',
+    './icons/icon-96x96.png',
+    './icons/icon-72x72.png'
 ];
 
 // Respond with cached resources
-self.addEventListener('fetch', function (e) {
+self.addEventListener("fetch", function(e) {
+    // cache all get requests to /api routes
+    if (e.request.url.includes("/api/")) {
+      e.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(e.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(e.request.url, response.clone());
+              }
+  
+              return response;
+            })
+        })
+      )}
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
         caches.match(e.request).then(function (request) {
